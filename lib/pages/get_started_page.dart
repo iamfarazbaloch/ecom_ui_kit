7be92f02575dd4440edd0ca932963134989796cd
Faraz Ahmad <faraz.ahmad0366@gmail.com>
2025-05-07 23:17:46 +0500

    complete get started page

--- lib/pages/get_started_page.dart
+++ lib/pages/get_started_page.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:ecom_ui_kit/widgets/my_button.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -10,6 +11,7 @@ class GetStartedPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          // Background image
           Container(
             decoration: const BoxDecoration(
               image: DecorationImage(
@@ -19,35 +21,41 @@ class GetStartedPage extends StatelessWidget {
             ),
           ),
 
+          // Blurred background for texts and button
           Positioned(
             bottom: 50,
             left: 20,
             right: 20,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text(
-                  'You want\nAuthentic, here\nyou go!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    'You want\nAuthentic, here\nyou go!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2,
+                    ),
                   ),
-                ),
-                const Text(
-                  'Find it here,buy it now',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-                Gap(40),
-                MyButton(
-                  text: 'Get Started',
-                  onTap: () {
-                    // Navigate or handle start
-                  },
-                ),
-              ],
+                  const Gap(10),
+                  const Text(
+                    'Find it here,buy it now',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  const Gap(40),
+                  MyButton(
+                    text: 'Get Started',
+                    onTap: () {
+                      // Navigate or handle start
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ],
