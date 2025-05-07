73c7be6020f0dc0cf41022fc271f976a289482c5
Faraz Ahmad <faraz.ahmad0366@gmail.com>
2025-05-07 22:22:36 +0500

    functionality

--- lib/widgets/my_search_field.dart
+++ lib/widgets/my_search_field.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 
-class MySearchField extends StatelessWidget {
+class MySearchField extends StatefulWidget {
   final IconData icon;
   final String hint;
   final TextEditingController controller;
+  final bool obscureText;
+
   const MySearchField({
     super.key,
     required this.icon,
     required this.hint,
     required this.controller,
+    required this.obscureText,
   });
+
+  @override
+  State<MySearchField> createState() => _MySearchFieldState();
+}
+
+class _MySearchFieldState extends State<MySearchField> {
+  late bool _obscure;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscure = widget.obscureText;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +35,26 @@ class MySearchField extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
       ),
       child: TextField(
-        controller: controller,
+        controller: widget.controller,
+        obscureText: _obscure,
         decoration: InputDecoration(
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-          prefixIcon: Icon(icon, color: Colors.grey),
-          hintText: hint,
+          prefixIcon: Icon(widget.icon, color: Colors.grey),
+          hintText: widget.hint,
+          suffixIcon:
+              widget.obscureText
+                  ? IconButton(
+                    icon: Icon(
+                      _obscure ? Icons.visibility_off : Icons.visibility,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscure = !_obscure;
+                      });
+                    },
+                  )
+                  : null,
         ),
       ),
     );
