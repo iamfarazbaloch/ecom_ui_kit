9154090c4514229640b8aebe9e129282f0d4acff
Faraz Ahmad <faraz.ahmad0366@gmail.com>
2025-05-07 23:25:15 +0500

    home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: []));
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.menu),
+                  Image.asset('assets/images/splash.png', width: 100),
+                  Image.asset('assets/icons/profile.png'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
